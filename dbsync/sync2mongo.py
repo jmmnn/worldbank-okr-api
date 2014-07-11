0c9b80b2850ec5b5b277f1b5db7a1e763e0cb7ed
nielsbantilan <niels.bantilan@gmail.com>
2014-07-11 17:34:48 -0400

    committing empty dbsync file, for future dev

---
+++ dbsync/sync2mongo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+------------------------------
+Open Knowledge Repository: OKR
+------------------------------
+
+Class for syncing query responses to Mongo Database.
+
+For more information go to:
+https://openknowledge.worldbank.org/harvesting-the-okr
+'''
+
